11908883e99b7d45bc91fb2c50bbd7e69d22deee
Ирина Чубаркина <ichubarkina@linguatrip.com>
2019-12-08 02:18:19 +0300

    it works!

--- laba_1.c
+++ laba_1.c
@@ -21,9 +21,9 @@ void help(const char fileName[]) {
 
 bool isValid( const char key[] )
 {
-    unsigned char c;
+    char c;
 
-    while ( ( c = *key ) && ( isalpha( c ) ) ) 
+    while ( (c = *key) && (isalpha(c)) ) 
       ++key;
 
     return *key == '\0'; 
@@ -36,7 +36,33 @@ FILE *openFileOrThrow(const char *fileName, const char *mode) {
         fprintf(stderr, "Cannot read file - %s\n", fileName); 
         exit(EXIT_FAILURE);
     }
+    return file;
 } 
+
+void fillVigenerTable(char vigenereTable[26][26]) {
+  
+  char alphabet[26];
+
+  for (int i = 0; i < 26; ++i)
+  {
+    alphabet[i] = i + 65;
+  }
+
+  int shift = 0;
+
+  for (int i = 0; i < 26; ++i)
+  {
+    for (int k = 0; k < 26; ++k)
+    {
+      char result = alphabet[k] + shift;
+
+      vigenereTable[i][k] = result <= 90 
+                ? result
+                : result - 26;
+    }
+    shift++;
+  }
+}
 
 int main (int argc, char *argv[])
 {
@@ -52,17 +78,21 @@ int main (int argc, char *argv[])
   int decode = 0;
   int keySet = 0;
 
+  char *key;
+
   while((opt = getopt(argc, argv, opts)) != -1) {
     switch(opt){
+
       case 'h':
         help(argv[0]);
         exit(EXIT_SUCCESS);
 
       case 'k':
         if  (!isValid(optarg)) {
-            fprintf(stderr, "\"%s\" - KEY is not valid\n%s", optarg, OFFER_HELP); 
+            fprintf(stderr, "\"%s\" - invalid KEY!\n%s", optarg, OFFER_HELP); 
             exit(EXIT_FAILURE);
         }
+        key =  optarg;
         keySet = 1;
         break;
 
@@ -85,7 +115,6 @@ int main (int argc, char *argv[])
     fprintf(stderr, "-k is required option\n%s", OFFER_HELP); 
     exit(EXIT_FAILURE);
   }
-
   FILE *source;
   FILE *destination;
 
@@ -105,7 +134,7 @@ int main (int argc, char *argv[])
 
       case 6:
         source = openFileOrThrow(argv[4], "r");
-        source = openFileOrThrow(argv[5], "w");
+        destination = openFileOrThrow(argv[5], "wb");
         
         break;
 
@@ -114,29 +143,104 @@ int main (int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     
-    char *str = (char*)malloc(sizeof(char));
-    int n = 0;
-    char ch;
+    char *tempString = (char*)malloc(sizeof(char));
+    int symbolCount = 0;
+    char symbol;
 
     while (1) 
     {
-        ch = fgetc(source);
+        symbol = fgetc(source);
 
-        if (ch == EOF)
+        if (symbol == EOF)
         {
           break;
         }
 
-        str[n] = ch;
-        n++;
-        str = (char*)realloc(str, sizeof(char)*(n + 1));
+        tempString[symbolCount] = symbol;
+        symbolCount++;
+        tempString = (char*)realloc(tempString, sizeof(char)*(symbolCount + 1));
     } 
-    str[n+1] = 0;
+    tempString[symbolCount+1] = 0;
 
-    printf("string - %s\n", str);
-
-fclose(source);
-fclose(destination);
+  char vigenereTable[26][26];
+  fillVigenerTable(vigenereTable);
   
-    return 0;
+  int keyLenght = strlen(key);  
+  for (int i = 0; i < keyLenght; ++i)
+  {
+    key[i] = toupper(key[i]);
+  }
+  printf("%s\n", key);
+
+  int keyPosition = 0;
+
+  for (int i = 0; i <= symbolCount; ++i)
+  {
+    if (isalpha(tempString[i]))
+    {
+      char symbol = toupper(tempString[i]); 
+
+      int symbolIndex = symbol - 65;
+      int keyIndex = key[keyPosition] - 65;
+
+      char encoded = vigenereTable[symbolIndex][keyIndex];
+
+      char c = fputc(encoded, destination);
+      if (c == EOF) {
+          fprintf(stderr, "Cannot write symbol to destination file\n"); 
+          exit(EXIT_FAILURE);
+      }
+      keyPosition = (keyPosition < keyLenght -1) 
+              ? keyPosition +1
+              : 0; 
+    }
+    else {
+
+      char c = fputc(tempString[i], destination);
+      if (c == EOF) {
+          fprintf(stderr, "Cannot write symbol to destination file\n"); 
+          exit(EXIT_FAILURE);
+        }
+    }
+  } 
+
+  // for (int i = 0; i <= symbolCount; ++i)
+  // {
+  //   if (isalpha(tempString[i]))
+  //   {
+  //     char symbol = toupper(tempString[i]); 
+
+  //     int keyIndex = key[keyPosition] - 65;
+
+  //     int k = 0;
+  //     while(vigenereTable[keyIndex][k] != symbol){
+  //         k++;
+  //     }
+  //     char iNeed = vigenereTable[0][k];
+
+  //     char c = fputc(iNeed, destination);
+  //     if (c == EOF) {
+  //         fprintf(stderr, "Cannot write symbol to destination file\n"); 
+  //         exit(EXIT_FAILURE);
+  //     }
+  //     keyPosition = (keyPosition < keyLenght -1) 
+  //             ? keyPosition +1
+  //             : 0; 
+  //   }
+  //   else {
+
+  //     char c = fputc(tempString[i], destination);
+  //     if (c == EOF) {
+  //         fprintf(stderr, "Cannot write symbol to destination file\n"); 
+  //         exit(EXIT_FAILURE);
+  //       }
+  //   }
+  // } 
+  fputc('\n', destination);
+
+  free(tempString);
+  fclose(source);
+  fclose(destination);
+    
+  return 0;
 }
